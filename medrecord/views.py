4ab09cc104432ef20e3eb086ea90523783f6372f
Izabela Popiel <izabelapopiel1@gmail.com>
2020-05-19 19:14:22 +0200

    saving clinics info

--- medrecord/views.py
+++ medrecord/views.py
@@ -14,26 +14,38 @@ def save_patient(request):
     if request.method == "POST":
         try:
             save_request_data(request)
+            messages.success(request, "Data saved successfully")
+
         except (ValueError, FileNotFoundError) as ex:
             messages.error(request, ex)
     return render(request, 'medrecord/save_patient.html')
 
 
 def save_request_data(request):
+    print(request)
     first_name = request.POST.get('first_name')
     last_name = request.POST.get('last_name')
     pesel = request.POST.get('pesel')
     if len(pesel) != 11:
         raise ValueError("PESEL length must be 11")
+    clinics = []
+    if 'cardiology' in request.POST:
+        clinics.append(request.POST.get('cardiology'))
+    if 'dentist' in request.POST:
+        clinics.append(request.POST.get('dentist'))
+    if 'orthopedic' in request.POST:
+        clinics.append(request.POST.get('orthopedic'))
+    if 'dermatology' in request.POST:
+        clinics.append(request.POST.get('dermatology'))
     chronic_conditions = request.POST.get('chronic_conditions')
     file_path = request.POST.get('file_path')
     if not os.path.isfile(file_path) and '.csv' not in file_path:
         raise FileNotFoundError('No such a csv file')
 
-    patient = [first_name, last_name, pesel, chronic_conditions]
+    patient = [first_name, last_name, pesel, ', '.join(clinics), chronic_conditions]
 
-    with open(file_path, mode='a') as record_file:
-        record_file = csv.writer(record_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    with open(file_path, mode='a', newline="") as record_file:
+        record_file = csv.writer(record_file, delimiter=',', quotechar='"')
         record_file.writerow(patient)
     return
 
@@ -56,13 +68,17 @@ def opened_record(request):
                 record_file = csv.reader(record_file, delimiter=',', quotechar='"')
                 record = []
                 for row in record_file:
-                    row_dict = {
-                        'first_name': row[0],
-                        'last_name': row[1],
-                        'pesel': row[2],
-                        'clinics': row[3],
-                        'chronic_conditions': row[4]
-                    }
+                    print(row)
+                    print('row')
+
+                    if row:
+                        row_dict = {
+                            'first_name': row[0],
+                            'last_name': row[1],
+                            'pesel': row[2],
+                            'clinics': row[3],
+                            'chronic_conditions': row[4]
+                        }
                     record.append(row_dict)
             return render(request, 'medrecord/opened_record.html', {'record': record})
         except (ValueError, FileNotFoundError) as ex:
